4f9729377a8e49d46026e00662560672995c1971
David Sandor <david.sandor@gmail.com>
2015-06-11 14:59:38 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-edge-sql
-=======
+edge-sql-plus
+=============
+
+This is an enhancement to the [edge-sql](http://tjanczuk.github.com/edge-sql) project by Tomasz Janczuk.  In `edge-sql-plus` I have added the ability to execute stored procedures and pass TVP or Table-Valued Parameters. In addition, it allows for muliple result sets (not MARS but rather multiple record sets).
+
+---
+
+
+##### Original Readme 
 
 This is a SQL compiler for edge.js. It allows accessing SQL databases from Node.js using Edge.js and ADO.NET. 
 
--- src/edge-sql/EdgeCompiler.cs
+++ src/edge-sql/EdgeCompiler.cs
@@ -4,6 +4,8 @@ using System.Dynamic;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.SqlServer.Server;
 
 public class EdgeCompiler
 {
@@ -53,8 +55,75 @@ public class EdgeCompiler
         {
             foreach (KeyValuePair<string, object> parameter in parameters)
             {
-                command.Parameters.AddWithValue(parameter.Key, parameter.Value ?? DBNull.Value);
+                if (parameter.Value.GetType() == typeof(ExpandoObject))
+                {
+                    if (((IDictionary<string, object>)parameter.Value).ContainsKey("UdtType"))
+                    {
+                        var dictionary = (IDictionary<string, object>)parameter.Value;
+
+                        var udtType = dictionary["UdtType"].ToString();
+                        var rows = dictionary["Rows"];
+                        var sqlDataRecords = CreateSqlDataRecords((IEnumerable<object>)rows);
+
+                        var commandParameter = command.Parameters.AddWithValue(parameter.Key, sqlDataRecords);
+                        commandParameter.SqlDbType = SqlDbType.Structured;
+                        commandParameter.TypeName = udtType;
+                    }
+                }
+                else
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value ?? DBNull.Value);
+                }
             }
+        }
+    }
+
+    private DataTable CreateDataTable(IEnumerable<object> values)
+    {
+        DataTable table = new DataTable();
+
+        var sampleFirstRow = values.FirstOrDefault();
+
+        if (sampleFirstRow == null) return table;
+
+        var row = sampleFirstRow as IDictionary<string, object>;
+
+        foreach (var key in row.Keys)
+        {
+            table.Columns.Add(key, row[key].GetType());
+        }
+
+        foreach (IDictionary<string, object> value in values)
+        {
+            table.Rows.Add(value.Values.ToArray<object>());
+        }
+
+        return table;
+    }
+
+    private IEnumerable<SqlDataRecord> CreateSqlDataRecords(IEnumerable<object> values)
+    {
+        var sampleFirstRow = values.FirstOrDefault();
+
+        if (sampleFirstRow == null) yield break;
+
+        var row = sampleFirstRow as IDictionary<string, object>;
+
+        var keyArray = row.Keys.ToArray<string>();
+
+        SqlMetaData[] metaData = new SqlMetaData[row.Keys.Count];
+
+        for (int i = 0; i < keyArray.Length; i++)
+        {
+            metaData[i] = new SqlMetaData(keyArray[i], GetSqlDbType(row[keyArray[i]].GetType()));
+        }
+
+        SqlDataRecord record = new SqlDataRecord(metaData);
+
+        foreach (IDictionary<string, object> value in values)
+        {
+            record.SetValues(value.Values.ToArray<object>());
+            yield return record;
         }
     }
 
@@ -71,45 +140,57 @@ public class EdgeCompiler
 
     async Task<object> ExecuteQuery(IDictionary<string, object> parameters, SqlCommand command, SqlConnection connection)
     {
-        List<object> rows = new List<object>();
+        List<object> recordsets = new List<object>();
+        
         this.AddParamaters(command, parameters);
         await connection.OpenAsync();
         using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.CloseConnection))
         {
             IDataRecord record = (IDataRecord)reader;
-            while (await reader.ReadAsync())
+            while (reader.HasRows)
             {
-                var dataObject = new ExpandoObject() as IDictionary<string, Object>;
-                var resultRecord = new object[record.FieldCount];
-                record.GetValues(resultRecord);
+                List<object> rows = new List<object>();
+                while (await reader.ReadAsync())
+                {
+                    var dataObject = new ExpandoObject() as IDictionary<string, Object>;
+                    var resultRecord = new object[record.FieldCount];
+                    record.GetValues(resultRecord);
 
-                for (int i = 0; i < record.FieldCount; i++)
-                {      
-                    Type type = record.GetFieldType(i);
-                    if (resultRecord[i] is System.DBNull)
+                    for (int i = 0; i < record.FieldCount; i++)
                     {
-                        resultRecord[i] = null;
-                    }
-                    else if (type == typeof(byte[]) || type == typeof(char[]))
-                    {
-                        resultRecord[i] = Convert.ToBase64String((byte[])resultRecord[i]);
-                    }
-                    else if (type == typeof(Guid) || type == typeof(DateTime))
-                    {
-                        resultRecord[i] = resultRecord[i].ToString();
-                    }
-                    else if (type == typeof(IDataReader))
-                    {
-                        resultRecord[i] = "<IDataReader>";
+                        Type type = record.GetFieldType(i);
+                        if (resultRecord[i] is System.DBNull)
+                        {
+                            resultRecord[i] = null;
+                        }
+                        else if (type == typeof(byte[]) || type == typeof(char[]))
+                        {
+                            resultRecord[i] = Convert.ToBase64String((byte[])resultRecord[i]);
+                        }
+                        else if (type == typeof(Guid) || type == typeof(DateTime))
+                        {
+                            resultRecord[i] = resultRecord[i].ToString();
+                        }
+                        else if (type == typeof(IDataReader))
+                        {
+                            resultRecord[i] = "<IDataReader>";
+                        }
+
+                        dataObject.Add(record.GetName(i), resultRecord[i]);
                     }
 
-                    dataObject.Add(record.GetName(i), resultRecord[i]);
+                    rows.Add(dataObject);
                 }
 
-                rows.Add(dataObject);
+                recordsets.Add(rows);
+
+                reader.NextResult();
             }
 
-            return rows;
+            if (recordsets.Count == 1)
+                return recordsets[0];
+            else
+                return recordsets;
         }
     }
 
@@ -142,5 +223,119 @@ public class EdgeCompiler
                 return await this.ExecuteQuery(parameters, command, connection);
             }
         }
+    }
+
+    public static Type GetClrType(SqlDbType sqlType)
+    {
+        switch (sqlType)
+        {
+            case SqlDbType.BigInt:
+                return typeof(long?);
+
+            case SqlDbType.Binary:
+            case SqlDbType.Image:
+            case SqlDbType.Timestamp:
+            case SqlDbType.VarBinary:
+                return typeof(byte[]);
+
+            case SqlDbType.Bit:
+                return typeof(bool?);
+
+            case SqlDbType.Char:
+            case SqlDbType.NChar:
+            case SqlDbType.NText:
+            case SqlDbType.NVarChar:
+            case SqlDbType.Text:
+            case SqlDbType.VarChar:
+            case SqlDbType.Xml:
+                return typeof(string);
+
+            case SqlDbType.DateTime:
+            case SqlDbType.SmallDateTime:
+            case SqlDbType.Date:
+            case SqlDbType.Time:
+            case SqlDbType.DateTime2:
+                return typeof(DateTime?);
+
+            case SqlDbType.Decimal:
+            case SqlDbType.Money:
+            case SqlDbType.SmallMoney:
+                return typeof(decimal?);
+
+            case SqlDbType.Float:
+                return typeof(double?);
+
+            case SqlDbType.Int:
+                return typeof(int?);
+
+            case SqlDbType.Real:
+                return typeof(float?);
+
+            case SqlDbType.UniqueIdentifier:
+                return typeof(Guid?);
+
+            case SqlDbType.SmallInt:
+                return typeof(short?);
+
+            case SqlDbType.TinyInt:
+                return typeof(byte?);
+
+            case SqlDbType.Variant:
+            case SqlDbType.Udt:
+                return typeof(object);
+
+            case SqlDbType.Structured:
+                return typeof(DataTable);
+
+            case SqlDbType.DateTimeOffset:
+                return typeof(DateTimeOffset?);
+
+            default:
+                throw new ArgumentOutOfRangeException("sqlType");
+        }
+    }
+
+    public static SqlDbType GetSqlDbType(Type clrType)
+    {
+        if (clrType == typeof(long) || clrType == typeof(long?))
+           return SqlDbType.BigInt;
+
+        if (clrType == typeof(byte[]))
+           return SqlDbType.Binary;
+
+        if (clrType == typeof(bool?) || clrType == typeof(bool))
+            return SqlDbType.Bit;
+
+        if (clrType == typeof(string))
+            return SqlDbType.VarChar;
+
+        if (clrType == typeof(DateTime) || clrType == typeof(DateTime?))
+            return SqlDbType.DateTime2;
+
+        if (clrType == typeof(double?) || clrType == typeof(double))
+            return SqlDbType.Float;
+
+        if (clrType == typeof(int?) || clrType == typeof(int))
+            return SqlDbType.Int;
+
+        if (clrType == typeof(float?) || clrType == typeof(float))
+            return SqlDbType.Real;
+
+        if (clrType == typeof(Guid?) || clrType == typeof(Guid))
+            return SqlDbType.UniqueIdentifier;
+
+        if (clrType == typeof(short?) || clrType == typeof(short))
+            return SqlDbType.SmallInt;
+
+        if (clrType == typeof(byte?) || clrType == typeof(byte))
+            return SqlDbType.TinyInt;
+
+        if (clrType == typeof(DataTable))
+            return SqlDbType.Structured;
+
+        if (clrType == typeof(DateTimeOffset) || clrType == typeof(DateTimeOffset?))
+            return SqlDbType.DateTimeOffset;
+
+        return SqlDbType.Variant;
     }
 }
